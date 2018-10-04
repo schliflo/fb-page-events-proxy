2d34b65f27fcd779641fd2dc1c65dafd62dec75c
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2018-10-04 17:20:00 +0200

    update example config

--- config-example.php
+++ config-example.php
@@ -1,8 +1,9 @@
 <?php
 return array(
-    'api_base_url' => 'https://graph.facebook.com/v2.11/',
+    'api_base_url' => 'https://graph.facebook.com/v3.1/',
     'app_id' => '1234567890',
     'app_secret' => 'abcdefgh1234567890',
+    'access_token' => 'abcdefgh1234567890',
     'page_id' => 'your-page',
     'allowed_origins' => [
         'http://localhost',
