defe03cf4f5dbae25ed76149bf3cc1e257aa55b7
Florian Schlittenbauer <florian.schlittenbauer@gmail.com>
2018-10-04 17:23:33 +0200

    describe config option

--- config-example.php
+++ config-example.php
@@ -2,7 +2,7 @@
 return array(
     'api_base_url' => 'https://graph.facebook.com/v3.1/',
     'app_id' => '1234567890',
-    'access_token' => 'abcdefgh1234567890',
+    'access_token' => 'abcdefgh1234567890', // use non-expiring token obtained like this: https://medium.com/@Jenananthan/how-to-create-non-expiry-facebook-page-token-6505c642d0b1
     'page_id' => 'your-page',
     'allowed_origins' => [
         'http://localhost',
